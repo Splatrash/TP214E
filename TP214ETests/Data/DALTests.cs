885c1f04ae50a998e9497aeb351c9b7bf504b617
William Francoeur <wfrancoeur@edu.cegepgarneau.ca>
2021-11-14 19:37:50 -0500

    Organisation des Tests Unitaires

--- TP214ETests/Data/DALTests.cs
+++ TP214ETests/Data/DALTests.cs
@@ -26,68 +26,8 @@ namespace TP214E.Data.Tests
             Assert.Fail();
         }
 
-        [TestMethod]
-        public void ObtenirNumeroCommande_retourne_le_Numero_de_commande_1_Si_la_Liste_De_Commandes_Est_Vide()
-        {
-            var commandes = Commandes.ListeCommandes;
-            var commande = new Commande(null, DateTime.Today);
-
-            
-            int numeroCommande = commande.ObtenirNumeroCommande();
 
 
-            Assert.AreEqual(1, numeroCommande);
-        }
-        [TestMethod]
-        public void ObtenirNumeroCommande_retourne_le_Numero_de_Commande_3_Si_la_Liste_De_Commandes_Contient_2_Commandes()
-        {
-            var commandes = Commandes.ListeCommandes;
-            var commande = new Commande(null, DateTime.Today);
 
-            commandes.Add(new Commande(null,DateTime.Today));
-            commandes.Add(new Commande(null,DateTime.Today));
-
-            int numeroCommande = commande.ObtenirNumeroCommande();
-
-
-            Assert.AreEqual(3, numeroCommande);
-            commandes.Clear();
-        }
-
-        [TestMethod]
-        public void CreationButtonsListeAliments_Retourne_Un_Bouton_d_Aliment_Dont_le_Tag_est_L_Aliment_En_Question()
-        {
-            //var aliment = new Mock<IAliment>();
-
-            //aliment.Setup(n => n.Nom).Returns("alimentTest");
-            //aliment.Setup(q => q.Quantite).Returns(1);
-            //aliment.Setup(u => u.Unite).Returns("ml");
-            //aliment.Setup(d => d.ExpireLe).Returns(DateTime.Today);
-
-            var aliment = new Aliment("alimentTest", 1, "ml", DateTime.Today);
-
-            Button btnCreer = new Button();
-
-            btnCreer = CreationControlButton.CreationButtonsListeAliments(btnCreer, aliment);
-
-            Assert.AreEqual(aliment.Nom, btnCreer.Content);
-
-        }
-
-        [TestMethod]
-        public void TypeDeBoutonACreer_Retourne_CreationButtonsListeAliments_Si_lObjet_En_Paramaetre_Est_Un_Aliment()
-        {
-            var aliment = new Mock<IAliment>();
-
-            aliment.Setup(n => n.Nom).Returns("alimentTest");
-            aliment.Setup(q => q.Quantite).Returns(1);
-            aliment.Setup(u => u.Unite).Returns("ml");
-            aliment.Setup(d => d.ExpireLe).Returns(DateTime.Today);
-
-            Button boutonCree =  CreationControlButton.TypeDeButtonACreer(aliment);
-            Button boutonCible = new Button();
-
-            Assert.AreEqual(aliment, boutonCree.Tag);
-        }
     }
 }
---
+++ TP214ETests/Data/ValidationsEntreesTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TP214E.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP214E.Data.Tests
+{
+    [TestClass()]
+    public class ValidationsEntreesTests
+    {
+        [TestMethod()]
+        public void ValiderAlphaNumeriqueAvecEspaceNonVide_Retourne_True_Si_La_Donne_En_Parametre_Respecte_Les_Contraintes()
+        {
+            string donneeEnParametre = "Ketchup";
+
+            bool validation = ValidationsEntrees.ValiderAlphaNumeriqueAvecEspaceNonVide(donneeEnParametre);
+
+            Assert.IsTrue(validation);
+        }
+
+        [TestMethod()]
+        public void ValiderAlphaNumeriqueAvecEspaceNonVide_Retourne_False_Si_La_Donne_En_Parametre_Ne_Respecte_Pas_Les_Contraintes()
+        {
+            string donneeEnParametre = "Ketchup!$/";
+
+            bool validation = ValidationsEntrees.ValiderAlphaNumeriqueAvecEspaceNonVide(donneeEnParametre);
+
+            Assert.IsFalse(validation);
+        }
+
+        [TestMethod()]
+        public void ValiderNumeriqueSansEspaceNonVide_Retourne_True_Si_La_Donne_En_Parametre_Respecte_Les_Contraintes()
+        {
+            string donneEnParametre = "12";
+
+            bool validation = ValidationsEntrees.ValiderNumeriqueSansEspaceNonVide(donneEnParametre);
+
+            Assert.IsTrue(validation);
+
+        }
+
+        [TestMethod()]
+        public void ValiderNumeriqueSansEspaceNonVide_Retourne_False_Si_La_Donne_En_Parametre_Ne_Respecte_Pas_Les_Contraintes()
+        {
+            string donneEnParametre = "pqowp";
+
+            bool validation = ValidationsEntrees.ValiderNumeriqueSansEspaceNonVide(donneEnParametre);
+
+            Assert.IsFalse(validation);
+
+        }
+
+        [TestMethod()]
+        public void ValiderAlphaNumeriqueSansEspaceNonVide_Retourne_True_Si_La_Donne_En_Parametre_Respecte_Les_Contraintes()
+        {
+            string donneEnParametre = "ml";
+
+            bool validation = ValidationsEntrees.ValiderNumeriqueSansEspaceNonVide(donneEnParametre);
+
+            Assert.IsTrue(validation);
+        }
+
+        [TestMethod()]
+        public void ValiderAlphaNumeriqueSansEspaceNonVide_Retourne_False_Si_La_Donne_En_Parametre_Ne_Respecte_Pas_Les_Contraintes()
+        {
+            string donneEnParametre = "ml   !";
+
+            bool validation = ValidationsEntrees.ValiderNumeriqueSansEspaceNonVide(donneEnParametre);
+
+            Assert.IsFalse(validation);
+        }
+    }
+}
